6b7b21f029982f5e85871681d6a417bf8f2b3af7
Wafiq Tafazzul Hilmi <wafiqhilmi4@gmail.com>
2023-09-27 09:36:38 +0700

    JUrnal 01

---
+++ Jurnal.java
@@ -0,0 +1,7 @@
+public class Jurnal {
+    public static void main(String[] args){
+        System.out.println("Nama: Wafiq Tafazzul Hilmi");
+        System.out.println("NIM: 607062300085");
+        System.out.println("Kelas: D3IF-47-04");
+    }
+}
